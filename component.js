abbe70783240fc6e7bb6bdb38ef4958d285ef188
Jack Franklin <jack@jackfranklin.net>
2015-05-25 17:16:20 +0100

    client side routing nearly

--- component.js
+++ component.js
@@ -1,4 +1,9 @@
 import React from 'react';
 
 export default class CannonComponent extends React.Component {
+  linkTo(to, children) {
+    return (
+      <a href={this.props.router.makeHref(to)}>{children}</a>
+    );
+  }
 }
