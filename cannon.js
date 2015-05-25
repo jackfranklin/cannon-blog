189e4bd9594f2524eccbad8ba5fec28c1d7af154
Jack Franklin <jack@jackfranklin.net>
2015-05-25 22:32:32 +0100

    Get static build generating

--- cannon.js
+++ cannon.js
@@ -3,5 +3,6 @@ require('babel/register');
 module.exports = {
   pages: require('./lib/pages'),
   init: require('./lib/init'),
-  render: require('./lib/render')
+  render: require('./lib/render'),
+  exposeRoutes: require('./lib/expose-routes')
 }
---
+++ script/build-static.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+require('babel/register');
+
+var webpack = require('webpack');
+var config = require('../lib/client/webpack.prod.config');
+var dumpHtml = require('../lib/static/build-site');
+var rmdir = require('rmdir');
+var fs = require('fs-extra');
+var path = require('path');
+
+rmdir('__out', function() {
+  console.log('Cannon generating production');
+  webpack(config, function(err, stats) {
+    console.log('Prod bundle generated');
+
+    var oldPath = path.join(process.cwd(), 'public', 'prod');
+    var newPath = path.join(process.cwd(), '__out', 'js');
+
+    dumpHtml().then(function() {
+      console.log('Static files generated into __out');
+
+      fs.copy(oldPath, newPath, function() {
+        console.log('Bundled JS moved into __out');
+      });
+    }).catch(function() {
+      console.log('ERROR', arguments);
+    });
+  });
+});
