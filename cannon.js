291bfa96f10fede8d88b24344b0c120a1dccb2e3
Jack Franklin <jack@jackfranklin.net>
2015-05-25 13:47:54 +0100

    Start the move to express like structure

--- cannon.js
+++ cannon.js
@@ -1,13 +1,3 @@
-import React from 'react';
-
-class Page extends React.Component {
-  linkTo(to, children) {
-    return (
-      <a href={this.props.router.makeHref(to)}>{children}</a>
-    );
-  }
-}
-
-export default {
-  Page
+module.exports = {
+  pages: require('./lib/pages')
 }
