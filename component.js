8ba827c6fcddbb5101ae7c9d46640d8fe5c748e7
Jack Franklin <jack@jackfranklin.net>
2015-05-31 19:26:51 +0100

    Add links to posts and tidy up route handling for blogs

--- component.js
+++ component.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import _ from 'lodash';
 import { Link } from 'react-router';
 
 export default class CannonComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.posts = _.map(_.filter(props.routes, function(route) {
+      return route.options && route.options.type === 'posts'
+    }), function(post) {
+      return _.extend(post.meta, {
+        path: post.path
+      });
+    });
+  }
+
   makeLink(props, child) {
     return (<Link {...props}>{child}</Link>);
   }
+
 }
