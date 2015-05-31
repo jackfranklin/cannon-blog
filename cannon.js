5ed0dc7658c782d959c1b1bed9e21c1f8f9f2c7b
Jack Franklin <jack@jackfranklin.net>
2015-05-31 19:04:45 +0100

    add support for basic blog posts

--- cannon.js
+++ cannon.js
@@ -4,5 +4,6 @@ module.exports = {
   pages: require('./lib/pages'),
   init: require('./lib/init'),
   render: require('./lib/render'),
-  exposeRoutes: require('./lib/expose-routes')
+  exposeRoutes: require('./lib/expose-routes'),
+  posts: require('./lib/posts')
 }
